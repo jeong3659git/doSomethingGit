871bee13eb9e77036612597ad0cf5fb7bc66da36
정민균 <samking3659@gmail.com>
2023-08-09 14:47:50 +0900

    230809 - 3) AppConfig 리팩토링   -- 중복제거 및 역할 구분

--- demo/src/main/java/hello/core/AppConfig.java
+++ demo/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,6 +12,27 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
+        return new MemberServiceImpl(
+                memberRepository()
+        );
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(
+            memberRepository(), discountPolicy()
+        );
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+/*
+    // 리팩토링 전
+    public MemberService memberService(){
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
@@ -18,8 +41,9 @@ public class AppConfig {
             new MemoryMemberRepository(),
             new FixDiscountPolicy()
         );
-    }
-
-
+    } 
+ */
 
 }
+// new MemoryMemberRepository() 이 부분이 중복 제거
+// MemoryMemberRepository 를 다른 구현체로 변경할 때 한 부분만 변경
