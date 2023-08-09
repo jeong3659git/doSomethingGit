841acaa857bd6569425e83dc38803efa2d2ce467
정민균 <samking3659@gmail.com>
2023-08-09 16:20:11 +0900

    230809 - 3) AppConfig - 할인 정책 변경 및 필기

--- demo/src/main/java/hello/core/AppConfig.java
+++ demo/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -28,7 +29,8 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+       // return new FixDiscountPolicy();
+       return new RateDiscountPolicy();   // -- 할인정책 변경시 구성요소에서 해당부분만 바꿔주면 사용영역에서 변경할 필요가 없다.
     }
 /*
     // 리팩토링 전
